82bfa77b2d340804f0e0c93158cd0daee0e26f9d
siontama <siontama@naver.com>
2021-10-06 14:37:27 +0900

    permutation

--- permutation/permutation.cpp
+++
@@ -1,6 +0,0 @@
-#include <algorithm>
-
-bool next_permutation(iterator first, iterator last);
-bool next_permutation(iterator first, iterator last, LessThanOrEqualFunction comp);
-bool prev_permutation(iterator first, iterator last);
-bool prev_permutation(iterator first, iterator last, LessThanOrEqualFunction comp);
